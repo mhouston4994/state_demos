5723df3ff29964ae686c721049a01aac5e178461
mhouston4994 <mhouston4994@gmail.com>
2019-10-17 12:49:36 -0600

    Refactored, changed listener to mouseOver, added ability to adjust each box

--- src/Boxes.js
+++ src/Boxes.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import './Boxes.css';
+import './ColorPicker.js';
 
 class Boxes extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            color: 'rgb(100, 100, 100)',
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    randomGenerator(){
+        let r = Math.floor(Math.random() * 256)
+        let g = Math.floor(Math.random() * 256)
+        let b = Math.floor(Math.random() * 256)
+        this.setState({color: `rgb(${r}, ${g}, ${b})`});
+    }
+    handleClick(){
+        this.randomGenerator();
+    }
     render(){
         return(
-            <div className="Boxes" style={{ backgroundColor: this.props.color }}></div>
+            <div  onMouseOver={this.handleClick}  className="Boxes">
+                <div className="Boxes" style={{ backgroundColor: this.state.color }}></div>
+                </div>
+
         )
     }
 }
--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -5,32 +5,16 @@ import './ColorPicker.css';
 class ColorPicker extends Component {
 
     static defaultProps = {
-        numBoxes: 16
+        numBoxes: 18
     }
-    constructor(props){
-        super(props);
-        this.state = {
-            color: 'rgb(100, 100, 100)',
-            numBoxes: Array.from({ length: this.props.numBoxes})
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
-    randomGenerator(){
-        let r = Math.floor(Math.random() * 256)
-        let g = Math.floor(Math.random() * 256)
-        let b = Math.floor(Math.random() * 256)
-        this.setState({color: `rgb(${r}, ${g}, ${b})`});
-    }
-    handleClick(){
-        this.randomGenerator();
-    }
+    
     render(){
+        const boxes = Array.from({ length: this.props.numBoxes }).map(() =>
+        <Boxes />);
         return(
             <div className="section">
-                <div  onClick={this.handleClick}  className="ColorPicker">
-                    {this.state.numBoxes.map(n => (
-                        <Boxes color={this.state.color}/>
-                    ))} 
+                <div className="ColorPicker">
+                        {boxes}
                 </div>
             </div>
         )
